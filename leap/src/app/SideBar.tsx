ca64cd909c78e3e8488636fe01eff46b1124e791
marcuscastelo <marcuscastelo@usp.br>
2024-11-13 20:01:54 -0300

    chore: format react files according to prettier

--- leap/src/app/SideBar.tsx
+++ leap/src/app/SideBar.tsx
@@ -1,11 +1,22 @@
 const profiles = [
-    "Joao", "Maria", "Jose", "Ana", "Pedro", "Paulo", "Lucas", "Marcos", "Lucia", "Marta"
+  'Joao',
+  'Maria',
+  'Jose',
+  'Ana',
+  'Pedro',
+  'Paulo',
+  'Lucas',
+  'Marcos',
+  'Lucia',
+  'Marta',
 ]
 
 export function SideBar() {
-    return (
-        <div className="w-40 bg-blue-900">
-            {profiles.map((profile) => <div className="bg-red-900 border border-black">{profile}</div>)}
-        </div>
-    )
+  return (
+    <div className="w-40 bg-blue-900">
+      {profiles.map((profile) => (
+        <div className="border border-black bg-red-900">{profile}</div>
+      ))}
+    </div>
+  )
 }
--- leap/src/app/page.tsx
+++ leap/src/app/page.tsx
@@ -1,21 +1,18 @@
-import { NavBar } from "./NavBar";
-import { SideBar } from "./SideBar";
+import { NavBar } from './NavBar'
+import { SideBar } from './SideBar'
 
 export default function Home() {
   return (
-    <div className="border border-purple-700 min-h-screen flex flex-col">
+    <div className="flex min-h-screen flex-col border border-purple-700">
       <NavBar />
-      <div className="flex flex-row flex-1">
+      <div className="flex flex-1 flex-row">
         <SideBar />
         <Recomendations />
       </div>
     </div>
-  );
+  )
 }
 
 function Recomendations() {
-  return (
-    <div className="flex-1 bg-yellow-900">
-    </div>
-  )
+  return <div className="flex-1 bg-yellow-900"></div>
 }
