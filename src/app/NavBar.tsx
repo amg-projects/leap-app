33fcf15697c673ea339fcc1a8fc97c04d223cba3
marcuscastelo <marcuscastelo@usp.br>
2024-11-18 21:34:15 -0300

    feature: NavBar gray blocks
    Co-authored-by: juninho01 <juninho01@users.noreply.github.com>
    Co-authored-by: gabriel <gabrielbless@users.noreply.github.com>

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,7 +1,11 @@
 export function NavBar() {
-  return <div className="min-h-16 bg-[#d9d9d9]"></div>
-}
-
-export function Outracois() {
-  return <>OUTRA COISA</>
+  return (
+    <div className="flex flex-row bg-[#d9d9d9] text-black">
+      <div className="m-3 size-8 rounded-xl bg-[#676767]"></div>
+      <div className="m-3 ml-5 h-8 w-24 rounded-2xl bg-[#676767]"></div>
+      <div className="m-3 ml-80 h-8 w-[630px] rounded-2xl bg-[#676767]"></div>
+      <div className="m-3 ml-36 mt-4 h-6 w-24 rounded-2xl bg-[#676767]"></div>
+      <div className="m-3 ml-20 h-8 w-24 rounded-2xl bg-[#676767]"></div>
+    </div>
+  )
 }
