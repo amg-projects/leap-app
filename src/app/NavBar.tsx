f80f4c58d0f8e57269c4791200433aab0591c0ec
gabriel <gabrielbarrientoalves@gmail.com>
2024-11-20 16:34:17 -0300

    sidebar done

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,11 +1,15 @@
 export function NavBar() {
   return (
     <div className="flex flex-row bg-[#d9d9d9] text-black">
-      <div className="m-3 size-8 rounded-xl bg-[#676767]"></div>
-      <div className="m-3 ml-5 h-8 w-24 rounded-2xl bg-[#676767]"></div>
-      <div className="m-3 ml-80 h-8 w-[630px] rounded-2xl bg-[#676767]"></div>
-      <div className="m-3 ml-36 mt-4 h-6 w-24 rounded-2xl bg-[#676767]"></div>
-      <div className="m-3 ml-20 h-8 w-24 rounded-2xl bg-[#676767]"></div>
+      <div className="flex flex-1 flex-row">
+        <div className="ml-3 size-8 self-center rounded-xl bg-[#676767]"></div>
+        <div className="ml-6 h-8 w-24 self-center rounded-2xl bg-[#676767]"></div>
+      </div>
+      <div className="m-3 h-8 w-[630px] rounded-2xl bg-[#676767]"></div>
+      <div className="flex flex-1 flex-row-reverse">
+        <div className="mr-3 h-8 w-24 self-center rounded-2xl bg-[#676767]"></div>
+        <div className="mr-28 h-6 w-24 self-center rounded-2xl bg-[#676767]"></div>
+      </div>
     </div>
   )
 }
--- src/app/SideBar.tsx
+++ src/app/SideBar.tsx
@@ -1,18 +1,83 @@
-const profiles: string[] = []
+'use client'
+import { useState } from 'react'
 
 export function SideBar({ expanded }: { expanded: boolean }) {
-  const sideBarWidthCollapsed = 'min-w-24'
+  const sideBarWidthCollapsed = 'min-w-10'
   const sideBarWidthExpanded = 'min-w-56'
 
-  const sideBarWidth = expanded ? sideBarWidthExpanded : sideBarWidthCollapsed
-
+  const [sideBar, setSideBar] = useState(false)
+  const sideBarWidth = sideBar ? sideBarWidthExpanded : sideBarWidthCollapsed
   return (
-    <div className={`bg-[#d9d9d9] ${sideBarWidth} `}>
-      {profiles.map((profile, idx) => (
-        <div key={idx} className="text-black">
-          {profile}
+    <>
+      <button onClick={() => setSideBar(!sideBar)}>a</button>
+      {sideBar.toString()}
+      {sideBar ? (
+        <ExpandedSideBarContent sideBarWidth={sideBarWidth} />
+      ) : (
+        <CollapsedSideBarContent sideBarWidth={sideBarWidth} />
+      )}
+    </>
+  )
+}
+function CollapsedSideBarContent({ sideBarWidth }: { sideBarWidth: string }) {
+  return (
+    <div className={`bg-[#d9d9d9] ${sideBarWidth} flex flex-col text-black `}>
+      <div className="flex flex-col">
+        <ChannelExample12 collapsed={true} />
+      </div>
+      <div className="mt-20 flex flex-col">
+        <ChannelExample12 collapsed={true} />
+      </div>
+    </div>
+  )
+}
+function ExpandedSideBarContent({ sideBarWidth }: { sideBarWidth: string }) {
+  return (
+    <div className={`bg-[#d9d9d9] ${sideBarWidth} flex flex-col text-black `}>
+      {/* <button onClick={() => setSideBar(!sideBar)}>a</button> */}
+      {/* {sideBar.toString()} */}
+      <div className="flex flex-col">
+        <div>FOLLOWED CHANNELS</div>
+        <ChannelExample12 collapsed={false} />
+      </div>
+      <div className="mt-20 flex flex-col">
+        RECOMMENDED CHANNELS
+        <ChannelExample12 collapsed={false} />
+      </div>
+    </div>
+  )
+}
+function ChannelExample12({ collapsed }: { collapsed: boolean }) {
+  return (
+    <>
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+      <ChannelExample collapsed={collapsed} />
+    </>
+  )
+}
+
+function ChannelExample({ collapsed }: { collapsed: boolean }) {
+  const className = collapsed ? 'hidden' : ''
+  return (
+    <div className="flex flex-row items-center border border-black">
+      <div className="m-1 size-8 rounded-full bg-black"></div>
+      <div className={`ml-1 flex-1 ${className}`}>
+        <div className="flex flex-row items-center">
+          <div className="flex-1">JOAOZINHOGG</div>
+          <div className="ml-2 mr-1 text-xs">🔴​14K</div>
         </div>
-      ))}
+        <div className="text-xs">ROBLOX</div>
+      </div>
     </div>
   )
 }
