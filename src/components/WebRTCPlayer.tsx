fb8b1fa7b32e48ec9469ffe3229b6cc5f8ac4852
marcuscastelo <marcuscastelo@usp.br>
2024-12-01 17:45:18 -0300

    WORKING BYPASS PERMS KATTA

--- src/components/WebRTCPlayer.tsx
+++ src/components/WebRTCPlayer.tsx
@@ -4,8 +4,8 @@
 import dynamic from 'next/dynamic'
 import { createRef, useEffect, useState } from 'react'
 
-const whepAPIUrl = 'http://[2804:14d:7e81:8a8f:b255:3176:a2d2:c21b]:6060/whep'
-// const whepAPIUrl = 'https://leap-webrtc.fly.dev/whep'
+// const whepAPIUrl = 'http://[2804:14d:7e81:8a8f:b255:3176:a2d2:c21b]:6060/whep'
+const whepAPIUrl = 'http://187.21.161.185:6060/whep'
 
 export const WebRTCPlayer = dynamic(() => Promise.resolve(WebRTCPlayerImpl), {
   ssr: false,
@@ -13,20 +13,51 @@ export const WebRTCPlayer = dynamic(() => Promise.resolve(WebRTCPlayerImpl), {
 
 const peerConnectionCache = new Map<string, RTCPeerConnection>()
 
+const pcConfig: RTCConfiguration = {
+  iceServers: [
+    {
+      urls: 'stun:187.21.161.185:3478',
+    },
+    {
+      urls: 'turn:187.21.161.185:3478',
+      username: 'username',
+      credential: 'password',
+    },
+  ],
+}
+
 function useRTCVideo(streamId: string) {
   const [mediaStream, setMediaStream] = useState<MediaStream | null>(null)
 
   useEffect(() => {
     const firstTime = !peerConnectionCache.has(streamId)
     const peerConnection =
-      peerConnectionCache.get(streamId) || new RTCPeerConnection()
+      peerConnectionCache.get(streamId) || new RTCPeerConnection(pcConfig)
     peerConnectionCache.set(streamId, peerConnection)
 
     const oldOnTrack = peerConnection.ontrack
     peerConnection.ontrack = function (event) {
+      // Renew the timeout every packet received
+
+      // event.streams[0].getTracks().forEach((track) => {
+      //   track.onended = () => {
+      //     setTimeout(() => window.location.reload(), 1000)
+      //   }
+      // })
+
       setMediaStream(event.streams[0])
       if (oldOnTrack) {
         oldOnTrack(event)
+      }
+    }
+
+    peerConnection.oniceconnectionstatechange = function () {
+      // if failed or disconnected, reload the page
+      if (
+        peerConnection.iceConnectionState === 'failed' ||
+        peerConnection.iceConnectionState === 'disconnected'
+      ) {
+        // setTimeout(() => window.location.reload(), 1000)
       }
     }
 
@@ -50,14 +81,19 @@ function useRTCVideo(streamId: string) {
         },
       })
         .then(async (response) => {
-          peerConnection.setRemoteDescription({
-            sdp: await response.text(),
-            type: 'answer',
-          })
+          peerConnection
+            .setRemoteDescription({
+              sdp: await response.text(),
+              type: 'answer',
+            })
+            .catch((err) => {
+              // Reload the page if the connection fails
+              console.dir(err)
+              setTimeout(() => window.location.reload(), 1000)
+            })
         })
         .catch((err) => {
           console.dir(err)
-          alert(err)
         })
     })
 
@@ -83,7 +119,7 @@ function WebRTCPlayerImpl({ streamId }: { streamId: string }) {
 
   // useEffect(() => {
   //   const intervalId = setInterval(() => {
-  //     window.location.reload(true); // Forces a hard reload
+  //     () => window.location.reload()(true); // Forces a hard reload
   //   }, 20000);
 
   //   return () => clearInterval(intervalId); // Cleanup interval on component unmount
