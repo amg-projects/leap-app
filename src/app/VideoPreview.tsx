f46c889777366ef7a5e7866aaa7b03037c2c6bfd
marcuscastelo <marcuscastelo@usp.br>
2024-12-01 17:49:33 -0300

    forgot in merge

--- src/app/VideoPreview.tsx
+++ src/app/VideoPreview.tsx
@@ -1,7 +1,12 @@
+import { WebRTCPlayer } from '@/components/WebRTCPlayer'
+
 export function VideoPreview({ className }: { className?: string }) {
+  console.log('VideoPreview render')
   return (
     <div
       className={`aspect-video rounded-2xl bg-[#c7d6cc] shadow-lg ${className}`}
-    ></div>
+    >
+      <WebRTCPlayer streamId="not-implemented" />
+    </div>
   )
 }
