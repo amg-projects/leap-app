c851d19e988ec2fbfe955ce3653240a691c1f8d6
marcuscastelo <marcuscastelo@usp.br>
2024-12-03 11:55:01 -0300

    chore: remove dead code

--- src/components/WebRTCPlayer.tsx
+++ src/components/WebRTCPlayer.tsx
@@ -4,7 +4,6 @@
 import dynamic from 'next/dynamic'
 import { createRef, useEffect, useState } from 'react'
 
-// const whepAPIUrl = 'http://[2804:14d:7e81:8a8f:b255:3176:a2d2:c21b]:6060/whep'
 const whepAPIUrl = 'http://187.21.161.185:6060/whep'
 
 export const WebRTCPlayer = dynamic(() => Promise.resolve(WebRTCPlayerImpl), {
@@ -37,27 +36,9 @@ function useRTCVideo(streamId: string) {
 
     const oldOnTrack = peerConnection.ontrack
     peerConnection.ontrack = function (event) {
-      // Renew the timeout every packet received
-
-      // event.streams[0].getTracks().forEach((track) => {
-      //   track.onended = () => {
-      //     setTimeout(() => window.location.reload(), 1000)
-      //   }
-      // })
-
       setMediaStream(event.streams[0])
       if (oldOnTrack) {
         oldOnTrack(event)
-      }
-    }
-
-    peerConnection.oniceconnectionstatechange = function () {
-      // if failed or disconnected, reload the page
-      if (
-        peerConnection.iceConnectionState === 'failed' ||
-        peerConnection.iceConnectionState === 'disconnected'
-      ) {
-        // setTimeout(() => window.location.reload(), 1000)
       }
     }
 
@@ -117,13 +98,6 @@ function WebRTCPlayerImpl({ streamId }: { streamId: string }) {
     }
   }, [videoRef, mediaStream])
 
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     () => window.location.reload()(true); // Forces a hard reload
-  //   }, 20000);
-
-  //   return () => clearInterval(intervalId); // Cleanup interval on component unmount
-  // }, []);
   return (
     <>
       <video ref={videoRef} autoPlay muted className={`w-full bg-black`} />
