5ef32b171db06dc2a74e4e6bf239a9b881a7706f
marcuscastelo <marcuscastelo@usp.br>
2024-12-18 21:54:42 -0300

    feature: simple tRPC test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,17 +1,24 @@
+'use client'
+import { trpc } from '../_trpc/client'
+
 export default function page() {
-  const messages = [
-    'oi, tudo bem?',
-    'tudo sim, e voce?',
-    'talvez eu mereca tudo isso',
-    'gary -1 ponto',
-  ]
+  const chat = trpc.chat.useQuery()
+  const mutation = trpc.newMessage.useMutation()
+  const messages = chat.data ?? []
 
   return (
-    <div className="h-screen border border-green-900 bg-black">
+    <div
+      className="h-screen border border-green-900 bg-black"
+      onClick={() => {
+        mutation.mutate({ message: 'hello' })
+        chat.refetch()
+      }}
+    >
       {messages.map(criaDiv)}
     </div>
   )
 }
+
 function criaDiv(texto: string, indice: number) {
   let className
   if (indice % 2 === 0) {
@@ -20,9 +27,8 @@ function criaDiv(texto: string, indice: number) {
     className = 'bg-white text-black'
   }
   return (
-    <div className={className}>
-      {texto}
-      {indice}
+    <div key={indice} className={className}>
+      {texto} - {indice}
     </div>
   )
 }
